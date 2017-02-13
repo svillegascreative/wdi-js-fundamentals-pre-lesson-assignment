e45177e96ee3f41697647412e15fd92a6103c910
sylvia villegas <sylvia@svillegas.com>
2017-02-12 23:04:20 -0500

    Completed Javascript assignment

---
+++ pacman-function.js
@@ -0,0 +1,7 @@
+function eatDot() {
+  console.log('MUNCH!');
+}
+
+var eatDot = function() {
+  console.log('MUNCH!');
+}
---
+++ pacman-objects-2.js
@@ -0,0 +1,4 @@
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' };
+console.log(ghosts);
+console.log(ghosts.inky);
+console.log(ghosts['inky']);
